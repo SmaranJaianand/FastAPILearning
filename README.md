7e0cebfe2a661940b5dab16f948ecd142afde7f9
smaran-jaianand <24070721037@sithyd.siu.edu.in>
2025-06-28 16:13:09 +0530

    app routes , dynamic routing , documentation , query params , post method using pydantic model

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # FastAPILearning
 I am learning FastAPI the web interface for python and wish to document my journey through Github.
+
+#### NOTE
+- learning.ipynb file is a personal reference file for me to keep track of my learning process.
+- i am referring to https://www.youtube.com/watch?v=7t2alSnE2-I&t=1277s for the learning process (primarily)
+- after this i will continue to refer to the official fastapi documentation for further learning and trackign using the ipynb file
+- the main file will solely be for the application and will only be used once i am in the project building phase
+- the main file as of now is just for learning purpose so the code may not be structured or might have redundant code
--- main.py
+++ main.py
@@ -1,12 +1,49 @@
 from fastapi import FastAPI
+from typing import Optional
+from pydantic import BaseModel
 
 app = FastAPI()
 
-@app.get('/')
-def index():
+
+@app.get('/') 
+def index(): 
 	return 'hello'
 
 
-@app.get('/about')
+@app.get('/about') # static routing
 def about():
 	return {'data':{'about page'}}
+
+
+# @app.get('/blog/unpublished') 
+# def show_unpublished():
+# 	return {'data': 'Unpublished blogs'}
+
+@app.get('/items/item-{item_id}') # dynamic routing
+async def read_item(item_id: int):
+	return {"item_id": item_id}
+
+@app.get('/blog/{id}')
+async def show_blog(id: int):
+	return {'data': id}
+
+@app.get('/blog?limit=10&published=true') # query parameters
+def show_blog():
+	return {'data': 'blog list'}
+
+
+@app.get('/blog') # query parameters
+async def show_blog(limit:int,published:bool):
+	if published:
+		return {'data': f'blog list of limit which are published : {limit}'}
+	else:
+		return {'data': f'blog list of limit: {limit}'}
+	
+class Blog(BaseModel): # pydantic model for blog
+	title: str
+	body: str
+	published: Optional[bool]
+
+@app.post('/blog') # post request
+async def create_blog(blog: Blog):
+	return {'bloginfo': f'Blog is created with title: {blog.title} and body: {blog.body} and published: {blog.published}','blog': blog} # returning the blog data as well
